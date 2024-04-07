bee1e2bb05dd140da0cd69a62df55ce343a66f8e
iboughtbed <testpythontosanzhar@gmail.com>
2024-04-07 21:54:21 +0500

    chore: setup components feat: add shadcn

---
+++ src/config/site.ts
@@ -0,0 +1,12 @@
+export const siteConfig = {
+  name: "Color palette",
+  url: "http://localhost:3000",
+  ogImage: "http://localhost:3000/og.jpg",
+  description: "Create, share and customize color palettes",
+  links: {
+    instagram: "https://instagram.com/iboughtbed",
+    github: "https://github.com/iboughtbed/n-factorial",
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
